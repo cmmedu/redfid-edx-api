a63155341ebfa1d03ff6c84e2fa174bd1cca534d
Vicente Daie Pinilla <vdaiep@gmail.com>
2024-05-27 17:10:51 -0400

    create user working

--- redfid_edx_user_manager/views.py
+++ redfid_edx_user_manager/views.py
@@ -3,10 +3,9 @@
 
 from django.conf import settings
 from django.contrib.auth import logout
-from django.http import HttpResponseRedirect, HttpResponseForbidden, Http404
+from django.http import HttpResponseRedirect, HttpResponseForbidden, Http404, HttpResponseBadRequest, HttpResponse
 from django.urls import reverse
 from django.views.generic.base import View
-from django.http import HttpResponse
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 import json
 import requests
@@ -19,24 +18,26 @@ class CreateRedfidUser(View):
     def post(self, request):
         
         from django.contrib.auth.models import User
-        from django.social_auth.models import UserSocialAuth
+        from social_django.models import UserSocialAuth
 
-        print(request.text)
-        user = request.user
-        if not user.email:
-            return HttpResponseForbidden("User email is required")
-        if not user.first_name:
-            return HttpResponseForbidden("User first name is required")
-        if not user.last_name:
-            return HttpResponseForbidden("User last name is required")
-        if not user.username:
-            return HttpResponseForbidden("User username is required")
-        password = "redfid"
-        new_user = User.objects.create_user(user.username, user.email, password, first_name=user.first_name, last_name=(user.first_name + " " + user.last_name))
-        new_user.save()
-        new_usersocialauth = UserSocialAuth.objects.create(user=new_user, provider='tpa-saml', uid="default:" + user.username, extra_data='{}')
-        new_usersocialauth.save()
-        return HttpResponse("User created successfully")
+        try:
+            print(request)
+            logger.info("CreateRedfidUser - request: {}".format(request))
+            data = json.loads(request.body)
+            username = data.get('username')
+            password = data.get('password')
+            email = data.get('email')
+            first_name = data.get('first_name')
+            last_name = data.get('last_name')
+            if not username or not password or not email or not first_name or not last_name:
+                return HttpResponseBadRequest("Missing required fields")
+            new_user = User.objects.create_user(username, email, password, first_name=first_name, last_name=last_name)
+            new_user.save()
+            new_usersocialauth = UserSocialAuth.objects.create(user=new_user, provider='tpa-saml', uid="default:" + username, extra_data='{}')
+            new_usersocialauth.save()
+            return HttpResponse(f"User {username} created successfully")
+        except json.JSONDecodeError:
+            return HttpResponseBadRequest("Invalid JSON data")
 
 
 class RedfidLogoutGet(View):
